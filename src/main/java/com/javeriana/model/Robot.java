2752e0697ad85df5f29b2a219b8869edf97bfa02
HauuF <Padre1000@outlook.es>
2024-04-09 19:58:41 -0500

    component & robot

--- src/main/java/com/javeriana/model/Robot.java
+++ src/main/java/com/javeriana/model/Robot.java
@@ -1,9 +1,68 @@
 package com.javeriana.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Robot {
 
+    private String code;
+    private double maxWeight;
+    private List<Component> components;
+    public String getCode() {
+        return code;
+    }
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public List<Component> getComponents() {
+        return this.components;
+    }
+
+    public Robot(String code, double maxWeight) {
+        this.code = code;
+        this.maxWeight = maxWeight;
+        this.components = new ArrayList<>();
+    }
+
+    public double getComponentsWeight(){
+        double totalWeight = 0.0;
+        for(int i = 0; i < components.size(); i++){
+            totalWeight += components.get(i).getWeight();
+        }
+        return totalWeight;
+    }
+
+    public boolean addComponent (int id, String name, double weight){
+
+        if (maxWeight >= getComponentsWeight() + weight) {
+            Component newComponent = new Component(id, name, weight);
+            components.add(newComponent);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public List<String> getComponentsNames() {
+        List<String> componentsNames = new ArrayList<>();
+        for (int i = 0; i < components.size(); i++) {
+            if (!componentsNames.contains(components.get(i).getName())) {
+                componentsNames.add(components.get(i).getName());
+            }
+        }
+        return componentsNames;
+    }
 }
