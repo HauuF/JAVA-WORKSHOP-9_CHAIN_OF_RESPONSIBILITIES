b364f5b9da711e1813cbac3ee0dfa93050317d3b
HauuF <Padre1000@outlook.es>
2024-04-09 20:39:34 -0500

    no complete

--- src/main/java/com/javeriana/model/Catalog.java
+++ src/main/java/com/javeriana/model/Catalog.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class Catalog {
 
+    private List<Robot> robots;
+    public Catalog() {
+        List<Robot> robots = new ArrayList<>();
+    }
+    public Robot searchRobotByCode(String code){
+        for (Robot robot : robots) {
+            if (robot.getCode().equals(code)) {
+                return robot;
+            }
+        }
+        return null;
+    }
 
 }
